8ecdb487c7a4f305aff8423720f321e301dbaf17
Jozsef Bali <70275083+Fenrir-Codes@users.noreply.github.com>
2023-06-06 20:38:12 +0200

    Ball bouncing update

--- AccelerationSensors/app/src/main/java/dk/tec/accelerationsensors/BallView.java
+++ AccelerationSensors/app/src/main/java/dk/tec/accelerationsensors/BallView.java
@@ -8,81 +8,78 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.view.View;
-import androidx.core.content.res.ResourcesCompat;
+
+import androidx.core.content.ContextCompat;
 
 public class BallView extends View implements SensorEventListener
 {
-    MainActivity main;
-    float xMove = 0.0f;
-    float yMove = 0.0f;
-    int xPos = 300;
-    int yPos = 300;
-    Drawable drwBall;
-    int drwWidth;
-    int drwHeight;
-    int viewWidth;
-    int viewHeigth;
+    MainActivity mainAct;
+    Drawable ball;
+    int ballWidth, ballHeight;
 
-    public BallView(MainActivity mainActivity)
-    {   super(mainActivity);
-        main = mainActivity;
+    float xPos = 300;
+    float yPos = 300;
+    float xMove, yMove;
+    int screenWidth;
+    int screenHeight;
+    public BallView(MainActivity mainAct) {
+        super(mainAct);
+        this.mainAct = mainAct;
 
-        drwBall = ResourcesCompat.getDrawable(getResources(), R.drawable.ball_basket, null);
-        drwWidth = drwBall.getIntrinsicWidth() / 5;
-        drwHeight = drwBall.getIntrinsicHeight() / 5;
-
+        ball = ContextCompat.getDrawable(mainAct.getApplicationContext(),R.drawable.ball_basket);
+        ballWidth = ball.getIntrinsicWidth()/10;
+        ballHeight = ball.getIntrinsicHeight()/10;
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-        viewWidth = w;
-        viewHeigth = h;
+        screenWidth = w;
+        screenHeight = h;
     }
 
     @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-
-        drwBall.setBounds(xPos, yPos, xPos + drwWidth, yPos + drwHeight);
-        drwBall.draw(canvas);
+    protected void onDraw(Canvas canvas)
+    {
+        //super.onDraw(canvas);
+        ball.setBounds((int)xPos, (int)yPos, ((int)xPos) + ballWidth, ((int)yPos) + ballHeight);
+        ball.draw(canvas);
     }
 
     @Override
     public void onSensorChanged(SensorEvent event)
     {
-        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            main.upDateValues(event.values);
+        if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
+        {
+            mainAct.upDateValues(event.values);
 
-            xMove = event.values[0];
-            yMove = event.values[1];
+            xMove += -event.values[0]/25;
+            xPos += xMove;
 
-            xPos -= xMove * 2;
-            yPos += yMove * 2;
+            yMove += event.values[1]/25;
+            yPos += yMove;
+            invalidate();
 
+            if(xPos < 0 - 50) {
+                xMove = Math.abs(xMove) * 0.9f;
+                xPos += xMove;
+            }
+            if(xPos > screenWidth - ballWidth + 5) {
+                xMove = Math.abs(xMove) * -0.9f;
+                xPos += xMove;
+            }
+            if(yPos < 0 - 10) {
+                yMove = Math.abs(yMove) * 0.9f;
+                yPos += yMove;
+            }
+            if(yPos > screenHeight - ballHeight + 15) {
+                yMove = Math.abs(yMove) * -0.9f;
+                yPos += yMove;
+            }
 
-            if (xPos <= 0) {
-                xPos = 0;
-                xMove = 0;  // Stop horizontal movement
-            }
-            if (xPos >= viewWidth - drwWidth) {
-                xPos = viewWidth - drwWidth;
-                xMove = 0;  // Stop horizontal movement
-            }
-            if (yPos <= 0) {
-                yPos = 0;
-                yMove = 0;  // Stop vertical movement
-            }
-            if (yPos >= viewHeigth - drwHeight) {
-                yPos = viewHeigth - drwHeight;
-                yMove = 0;  // Stop vertical movement
-            }
         }
-        invalidate();
     }
 
     @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-    }
+    public void onAccuracyChanged(Sensor sensor, int accuracy) { }
 }
